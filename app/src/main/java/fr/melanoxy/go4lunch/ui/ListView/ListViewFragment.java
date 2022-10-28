205db85926d26d026cda0ee2947fb828401c4759
MattAzerty <maguillaud@laposte.net>
2022-10-28 16:48:21 +0200

    #02 - Bottom navigation with 3 blank fragments

---
+++ app/src/main/java/fr/melanoxy/go4lunch/ui/ListView/ListViewFragment.java
@@ -0,0 +1,29 @@
+package fr.melanoxy.go4lunch.ui.ListView;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import fr.melanoxy.go4lunch.R;
+
+
+public class ListViewFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_list_view, container, false);
+
+        return view;
+    }
+}
